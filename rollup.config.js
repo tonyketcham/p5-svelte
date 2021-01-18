26f33568f69a5d381d8662b46b1396b90f0ad4ff
tonyketcham <ketcham@pnw.edu>
2021-01-18 11:45:51 -0600

    Reduce bundle size by explicitly removing p5 from bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import includePaths from 'rollup-plugin-includepaths';
 import pkg from './package.json';
 
 const name = pkg.name
@@ -8,11 +8,19 @@ const name = pkg.name
   .replace(/^\w/, (m) => m.toUpperCase())
   .replace(/-\w/g, (m) => m[1].toUpperCase());
 
+let includePathOptions = {
+  paths: ['es6'],
+  include: {
+    p5: './lib/p5.min.js', // include library in es6 modules
+  },
+  external: ['p5'], // but don't bundle it in bundle.js
+};
+
 export default {
   input: 'src/index.js',
   output: [
     { file: pkg.module, format: 'es' },
     { file: pkg.main, format: 'umd', name },
   ],
-  plugins: [svelte(), nodeResolve(), commonjs()],
+  plugins: [svelte(), nodeResolve(), includePaths(includePathOptions)],
 };
