93476cf67fba5344d341473fdf2b7681168480a7
tonyketcham <tony@karmalies.studio>
2022-04-02 19:30:15 -0500

    extend codeblock component w/ svelte-ts case

--- src/helpers/sketchExampleCode.ts
+++ src/helpers/sketchExampleCode.ts
@@ -1,9 +1,21 @@
-export function sketchExampleCode(sketch: string): string {
-	return `<script>
-  import P5 from "$lib/P5.svelte";
+export type Language = 'svelte' | 'svelte-ts' | 'ts' | 'js';
+
+export function sketchExampleCode(sketch: string, lang: Language): string {
+	if (lang === 'svelte') {
+		return `<script>
+  import P5 from "p5-svelte";
   
   const sketch = ${sketch}
 </script>
 
 <P5 {sketch} />`;
+	} else if (lang === 'svelte-ts') {
+		return `<script lang="ts">
+  import P5, { type Sketch } from "p5-svelte";
+
+  const sketch: Sketch = ${sketch}
+</script>
+
+<P5 {sketch} />`;
+	}
 }
